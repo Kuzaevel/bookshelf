7c3350cec048c2b2efd0da0d01b62d6a9b648168
kuzaev <kuzaeveldar@gmail.com>
2018-10-29 23:33:02 +0500

    fix js code

--- src/js/app.js
+++ src/js/app.js
@@ -1,11 +1,8 @@
 jQuery( document ).ready(function( $ ) {
-
-init();
-
+    init();
 });
 
 function init(){
-    console.log('start init');
 
     $('.new-book').click(function (e) {
         e.preventDefault();
@@ -17,11 +14,49 @@ function init(){
 
     $('.btn-add-book').click(function (e) {
         e.preventDefault();
-        var form_data = $('.form-add_boo').serialize();
-        console.log(form_data);
-
+        var form_data = $('.form-add_book').serialize();
         $.post('/add',form_data,function(response) {
             $(".restore-block").html(response).after(init());
         });
-    })
+    });
+
+    $('.btn-view_book').click(function (e) {
+        e.preventDefault();
+        var post_url = $(this).attr("data-link-atr");
+        $.get(post_url,function(response) {
+            $(".restore-block").html(response).after(init());
+        });
+    });
+
+    $('.btn-edit_book').click(function (e) {
+        e.preventDefault();
+        var post_url = $(this).attr("data-link-atr");
+        $.get(post_url,function(response) {
+            $(".restore-block").html(response).after(init());
+        });
+    });
+
+    $('.form-edit_book').submit(function (e) {
+        e.preventDefault();
+        var form_data = $(this).serialize();
+        var post_url = $(this).attr("action");
+        $.post(post_url,form_data,function(response) {
+            $(".restore-block").html(response).after(init());
+        });
+    });
+
+    $('.btn-delete_book').click(function (e) {
+        e.preventDefault();
+        var post_url = $(this).attr("data-link-atr");
+        $.get(post_url,function(response) {
+            $(".restore-block").html(response).after(init());
+        });
+    });
+
+    $('.btn-back-to-table').click(function (e) {
+        e.preventDefault();
+        $.post('/',function(response) {
+            $(".restore-block").html(response).after(init());
+        });
+    });
 }
