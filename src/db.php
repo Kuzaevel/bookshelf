68831c18e1bdabc5868551b89e9251982d01cbdb
kuzaev <kuzaeveldar@gmail.com>
2018-10-29 12:25:00 +0500

    add setting files

--- src/db.php
+++ src/db.php
@@ -5,9 +5,6 @@
  * Date: 03.06.18
  * Time: 3:18
  */
-
-require_once '../src/db.php';
-
     class db{
         //properties
         private $dbhost  = "localhost";
@@ -16,12 +13,13 @@ require_once '../src/db.php';
         private $dbname  = "bookshelf";
         private $charset = "utf8";
 
+
         //connection
         public function connect(){
             $mysql_connect_str = "mysql:host=$this->dbhost;dbname=$this->dbname;charset=$this->charset";
             $dbConection = new PDO($mysql_connect_str,$this->dbuser,$this->dbpass);
             $dbConection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-            $dbConection->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
             return $dbConection;
         }
+
     };
